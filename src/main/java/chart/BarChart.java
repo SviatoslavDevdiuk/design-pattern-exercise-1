98007beb68e7eb1e5caecb913445d1f0e7dcad8f
Sviatoslav Devdiuk <sviatoslavdevdiuk@gmail.com>
2019-02-17 12:07:15 +0100

    implementing of Fluent Builder

--- src/main/java/chart/BarChart.java
+++ src/main/java/chart/BarChart.java
@@ -5,20 +5,19 @@ import java.util.List;
 public class BarChart {
 
     private String title;
-
     private String fontName;
     private int fontSize;
     private String fontColor;
-
     private int width;
     private int height;
     private String backgroundColor;
-
     private List<String> data;
-
     private boolean displayTitle;
     private boolean displayLegend;
 
+    public BarChart() {
+
+    }
 
     public BarChart(String title, String fontName, int fontSize, String fontColor, int width, int height, String backgroundColor, List<String> data, boolean displayTitle, boolean displayLegend) {
         this.title = title;
@@ -124,5 +123,89 @@ public class BarChart {
 
     public void setDisplayLegend(boolean displayLegend) {
         this.displayLegend = displayLegend;
+    }
+
+    public static class BarChartBuilder {
+
+        private String title;
+        private String fontName;
+        private int fontSize;
+        private String fontColor;
+        private int width;
+        private int height;
+        private String backgroundColor;
+        private List<String> data;
+        private boolean displayTitle;
+        private boolean displayLegend;
+
+        public BarChartBuilder(String title) {
+            this.title = title;
+        }
+
+        public BarChartBuilder withTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public BarChartBuilder withFontName(String fontName) {
+            this.fontName = fontName;
+            return this;
+        }
+
+        public BarChartBuilder withFontSize(int fontSize) {
+            this.fontSize = fontSize;
+            return this;
+        }
+
+        public BarChartBuilder withFontColor(String fontColor) {
+            this.fontColor = fontColor;
+            return this;
+        }
+
+        public BarChartBuilder withWidth(int width) {
+            this.width = width;
+            return this;
+        }
+
+        public BarChartBuilder withHeight(int height) {
+            this.height = height;
+            return this;
+        }
+
+        public BarChartBuilder withBackgroundColor(String backgroundColor) {
+            this.backgroundColor = backgroundColor;
+            return this;
+        }
+
+        public BarChartBuilder withData(List<String> data) {
+            this.data = data;
+            return this;
+        }
+
+        public BarChartBuilder withDisplayTitle(boolean displayTitle) {
+            this.displayTitle = displayTitle;
+            return this;
+        }
+
+        public BarChartBuilder withDisplayLegend(boolean displayLegend) {
+            this.displayLegend = displayLegend;
+            return this;
+        }
+
+        public BarChart build() {
+
+            BarChart barChart = new BarChart();
+            barChart.setTitle(this.title);
+            barChart.setBackgroundColor(this.backgroundColor);
+            barChart.setData(this.data);
+            barChart.setDisplayLegend(this.displayLegend);
+            barChart.setFontColor(this.fontColor);
+            barChart.setFontName(this.fontName);
+            barChart.setFontSize(this.fontSize);
+            barChart.setHeight(this.height);
+            barChart.setWidth(this.width);
+
+            return barChart;
+        }
     }
 }
